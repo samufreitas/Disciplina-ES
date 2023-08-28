58615a074fc85f2505f888effd37661eb3594190
samufreitas <samuel.freitas@sou.ufac.br>
2023-08-28 10:41:12 -0500

    Função de logout pronta e funcionando para todos os perfios!

--- Projeto/settings.py
+++ Projeto/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-pd*fhe5x3$2z!$$veqr3@8tz5b8*%7b$!+8pc^cip9!7x_u&_8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -151,10 +151,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Configurações de envio do e-mail
 
 # Em produção
-#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+"""EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')"""
